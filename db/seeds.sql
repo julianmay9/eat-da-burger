615cc16270033461cdf392251001faabd5400ca0
Julian <julian@Julians-MacBook-Pro-3.local>
2019-05-21 19:01:54 -0400

    eat da burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Beef', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chargrilled Chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Garlic Mayo', false);
